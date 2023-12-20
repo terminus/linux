dad204c6f1860806a8e7d7927832ae6fff38df83
Ankur Arora <ankur.a.arora@oracle.com>
2023-12-20 15:40:31 -0800

    sched: handle preempt=voluntary under PREEMPT_AUTO
    The default preemption policy for voluntary preemption under
    PREEMPT_AUTO is to schedule eagerly for tasks of higher scheduling
    class, and lazily for well-behaved, non-idle tasks.
    
    This is the same policy as preempt=none, with an eager handling of
    higher priority scheduling classes.
    
    Comparing a cyclictest workload with a background kernel load of
    'stress-ng --mmap', shows that both the average and the maximum
    latencies improve:
    
     # stress-ng --mmap 0 &
     # cyclictest --mlockall --smp --priority=80 --interval=200 --distance=0 -q -D 300
    
                                         Min     (  %stdev )    Act     (  %stdev )   Avg     (  %stdev )   Max      (  %stdev )
    
      PREEMPT_AUTO, preempt=voluntary    1.73  ( +-  25.43% )   62.16 ( +- 303.39% )  14.92 ( +-  17.96% )  2778.22 ( +-  15.04% )
      PREEMPT_DYNAMIC, preempt=voluntary 1.83  ( +-  20.76% )  253.45 ( +- 233.21% )  18.70 ( +-  15.88% )  2992.45 ( +-  15.95% )
    
    The table above shows the aggregated latencies across all CPUs.
    
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Ziljstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Originally-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/lkml/87jzshhexi.ffs@tglx/
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1052,6 +1052,9 @@ static resched_t resched_opt_translate(struct task_struct *curr,
 	if (preempt_model_preemptible())
 		return RESCHED_NOW;
 
+	if (preempt_model_voluntary() && opt == RESCHED_PRIORITY)
+		return RESCHED_NOW;
+
 	if (is_idle_task(curr))
 		return RESCHED_NOW;
 
@@ -2289,7 +2292,7 @@ void wakeup_preempt(struct rq *rq, struct task_struct *p, int flags)
 	if (p->sched_class == rq->curr->sched_class)
 		rq->curr->sched_class->wakeup_preempt(rq, p, flags);
 	else if (sched_class_above(p->sched_class, rq->curr->sched_class))
-		resched_curr(rq);
+		resched_curr_priority(rq);
 
 	/*
 	 * A queue event has occurred, and we're going to schedule.  In
@@ -8989,11 +8992,11 @@ static void __sched_dynamic_update(int mode)
 	case preempt_dynamic_none:
 		if (mode != preempt_dynamic_mode)
 			pr_info("%s: none\n", PREEMPT_MODE);
-		preempt_dynamic_mode = mode;
 		break;
 
 	case preempt_dynamic_voluntary:
-		preempt_dynamic_mode = preempt_dynamic_undefined;
+		if (mode != preempt_dynamic_mode)
+			pr_info("%s: voluntary\n", PREEMPT_MODE);
 		break;
 
 	case preempt_dynamic_full:
@@ -9003,9 +9006,10 @@ static void __sched_dynamic_update(int mode)
 
 		if (mode != preempt_dynamic_mode)
 			pr_info("%s: full\n", PREEMPT_MODE);
-		preempt_dynamic_mode = mode;
 		break;
 	}
+
+	preempt_dynamic_mode = mode;
 }
 
 #endif /* CONFIG_PREEMPT_AUTO */
